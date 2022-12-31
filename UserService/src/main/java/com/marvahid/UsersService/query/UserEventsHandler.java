85dbb498890f96876816626693cfcb693c5adcb6
Marzieh Vahid <marzieh.vahid@gmail.com>
2022-12-31 11:32:38 +0330

    add UserService

---
+++ UserService/src/main/java/com/marvahid/UsersService/query/UserEventsHandler.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marvahid.UsersService.query;
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+import com.marvahid.coremodule.model.PaymentDetails;
+import com.marvahid.coremodule.model.User;
+import com.marvahid.coremodule.query.FetchUserPaymentDetailsQuery;
+ 
+
+@Component
+public class UserEventsHandler {
+
+    @QueryHandler
+    public User findUserPaymentDetails(FetchUserPaymentDetailsQuery query) {
+        
+        PaymentDetails paymentDetails = new PaymentDetails("123Card","123"
+        ,12,2030,"Marzieh Vahid");
+
+        User user = new User("Marzieh","Vahid",query.getUserId(),paymentDetails);
+        return user;
+    }
+    
+    
+}
