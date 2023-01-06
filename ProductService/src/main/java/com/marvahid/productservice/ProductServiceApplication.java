b174a511efc512e8465dfe7b211959a5f4043752
Marzieh Vahid <marzieh.vahid@gmail.com>
2023-01-06 09:22:27 +0330

    create snapshot for productService

--- ProductService/src/main/java/com/marvahid/productservice/ProductServiceApplication.java
+++ ProductService/src/main/java/com/marvahid/productservice/ProductServiceApplication.java
@@ -6,11 +6,15 @@ import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfiguration;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -32,5 +36,10 @@ public class ProductServiceApplication {
                 configuration -> new ProductServiceEventsErrorHandler());
 //        configurer.registerListenerInvocationErrorHandler("product-group",
 //                configuration ->  PropagatingErrorHandler.instance());
+    }
+
+    @Bean(name = "productSnapshotTriggerDefinition")
+    public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 3);//threshold number based on project
     }
 }
